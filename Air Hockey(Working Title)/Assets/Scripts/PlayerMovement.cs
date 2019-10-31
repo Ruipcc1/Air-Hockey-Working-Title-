a091c28a1f0ddf279e5b78a700b13da6f4a027a4
Rui <ruipcc1@gmail.com>
2019-10-31 19:05:36 +0000

    Striker's movement restricted to its own playing field.

--- Air Hockey(Working Title)/Assets/Scripts/PlayerMovement.cs
+++ Air Hockey(Working Title)/Assets/Scripts/PlayerMovement.cs
@@ -10,11 +10,30 @@ public class PlayerMovement : MonoBehaviour
     Vector2 playerSize;
 
     Rigidbody2D rb;
+
+    public Transform BoundaryHolder;
+
+    Boundary playerBoundary;
+
+    struct Boundary
+    {
+        public float Up, Down, Left, Right;
+
+        public Boundary(float up, float down, float left, float right)
+        {
+            Up = up; Down = down; Left = left; Right = right;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         playerSize = GetComponent<SpriteRenderer>().bounds.extents;
         rb = GetComponent<Rigidbody2D>();
+
+        playerBoundary = new Boundary(BoundaryHolder.GetChild(0).position.y,
+                                      BoundaryHolder.GetChild(1).position.y,
+                                      BoundaryHolder.GetChild(2).position.x,
+                                      BoundaryHolder.GetChild(3).position.x);
     }
 
     // Update is called once per frame
@@ -43,7 +62,11 @@ public class PlayerMovement : MonoBehaviour
 
             if (canMove)
             {
-                rb.MovePosition(mousePos);
+                Vector2 clampedMousePos = new Vector2(Mathf.Clamp(mousePos.x, playerBoundary.Left,
+                                                                    playerBoundary.Right),
+                                                     Mathf.Clamp(mousePos.y, playerBoundary.Down,
+                                                                    playerBoundary.Up));
+                rb.MovePosition(clampedMousePos);
             }
         }
         else
