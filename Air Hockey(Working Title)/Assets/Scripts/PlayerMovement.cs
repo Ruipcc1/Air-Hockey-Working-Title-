44d016072df2c2db31a28763d11fd72bbecd3faa
Rui <ruipcc1@gmail.com>
2019-10-31 20:40:37 +0000

    Started Development of enemy AI(File Missing)

--- Air Hockey(Working Title)/Assets/Scripts/PlayerMovement.cs
+++ Air Hockey(Working Title)/Assets/Scripts/PlayerMovement.cs
@@ -15,15 +15,7 @@ public class PlayerMovement : MonoBehaviour
 
     Boundary playerBoundary;
 
-    struct Boundary
-    {
-        public float Up, Down, Left, Right;
-
-        public Boundary(float up, float down, float left, float right)
-        {
-            Up = up; Down = down; Left = left; Right = right;
-        }
-    }
+    
     // Start is called before the first frame update
     void Start()
     {
