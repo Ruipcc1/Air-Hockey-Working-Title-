307cb72843507000fed0e9a179fc818ca7dc03dd
Unknown <ruipcc1@gmail.com>
2019-11-11 17:51:29 +0000

    Added Prototype Menu and selecting different modes

---
+++ Air Hockey(Working Title)/Assets/Scripts/MainScene/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioClip[] PuckCollision;
+    public AudioClip Goal;
+    public AudioClip BlueWins;
+    public AudioClip RedWins;
+
+    private AudioSource audioSource;
+
+    private AudioClip randomPick;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayPuckCollision()
+    {
+        randomPick = PuckCollision[Random.Range(0, PuckCollision.Length)];
+        audioSource.PlayOneShot(randomPick);
+    }
+
+    public void PlayGoal()
+    {
+        audioSource.PlayOneShot(Goal);
+    }
+
+    public void PlayLostGame()
+    {
+        audioSource.PlayOneShot(BlueWins);
+    }
+
+    public void PlayWonGame()
+    {
+        audioSource.PlayOneShot(RedWins);
+    }
+}
---
+++ Air Hockey(Working Title)/Assets/Scripts/MenuScenes/LocalManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LocalManager : MonoBehaviour
+{
+    public void PlayvsAI()
+    {
+        SceneManager.LoadScene("PracticeMode");
+    }
+    public void PlayvsPlayer()
+    {
+        SceneManager.LoadScene("LocalMultiplayer");
+    }
+}
